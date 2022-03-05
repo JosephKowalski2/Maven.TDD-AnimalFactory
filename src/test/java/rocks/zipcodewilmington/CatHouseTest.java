8cf31f293a0ac589ea6f4c6bb5f0cc42630459a3
Joseph Kowalski <jkjr25@gmail.com>
2022-03-05 15:42:51 -0500

    Added cat house add cat test

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,16 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
+import static java.util.Calendar.JANUARY;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +20,21 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+    @Test
+    public void addCatTest() {
+        // Given
+        Cat cat = new Cat(null,null,null);
+        Integer expectedNumberofCats = 3;
+
+        // When
+        CatHouse.add(cat);
+        CatHouse.add(cat);
+        CatHouse.add(cat);
+        Integer actualNumberofCats = CatHouse.getNumberOfCats();
+
+        // Then
+        Assert.assertEquals(expectedNumberofCats, actualNumberofCats);
+    }
+
 }
